b63b26c42923a5f34fb7e2d6bac71bd610fb845e
tien-tkc <cattientk92@gmail.com>
2017-08-09 10:32:10 +0700

    first commit

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,22 @@
+package com.example.demo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+open class DemoApplication
+
+@RestController
+class HelloControler() {
+    @GetMapping("/{name}")
+    fun hello(@PathVariable(required = false) name: String) = "hello ,$name"
+}
+
+
+fun main(args: Array<String>) {
+    SpringApplication.run(DemoApplication::class.java, *args)
+}
